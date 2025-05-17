a2565dd893ac07a45b42f1bd960f18d00bf7229e
tributegames <tributegamestudio@gmail.com>
2025-05-17 19:22:33 +0200

    Add event channels for unity generic types

---
+++ Assets/Scripts/Core/Events/Generic/Vector2EventChannelSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Vector2EventChannelSO", menuName = "Events/Generic/Vector2 Event Channel")]
+public class Vector2EventChannelSO : GenericEventChannelSO<Vector2>
+{
+
+}
---
+++ Assets/Scripts/Core/Events/Generic/VoidEventChannelSO.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "VoidEventChannelSO", menuName = "Events/Generic/Void Event Channel")]
+public class VoidEventChannelSO : GenericEventChannelSO<VoidEvent>
+{
+}
