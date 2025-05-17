4ea28e267db394f1d9805b1c5af831fb7a762f98
tributegames <tributegamestudio@gmail.com>
2025-05-17 19:36:11 +0200

    Create a basic Input Manager to demonstrate the event channel workflow

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    [Header("Outbound Communication")]
+    //This takes a scriptable object of type Vector2EventChannelSO to send Vector2 Events
+    [SerializeField] Vector2EventChannelSO moveEventChannel;
+    [SerializeField] BoolEventChannelSO jumpEventChannel;
+    InputAction moveAction;
+    InputAction jumpAction;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    void Update()
+    {
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+
+        // Raising an event of type moveValue
+        Helpers.RaiseIfNotNull(moveEventChannel, moveValue);
+
+        //Raise a bool event to represent the Jump key being pressed
+        Helpers.RaiseIfNotNull(jumpEventChannel, jumpAction.IsPressed());
+    }
+}
