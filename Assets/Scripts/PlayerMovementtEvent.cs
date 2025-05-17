9ff5958802999411cf8b62dec0fa3dfd9ed01dd6
Sinon <coolguy6318@hotmail.com>
2025-05-17 22:02:09 +0200

    Trying Movement

---
+++ Assets/Scripts/PlayerMovementtEvent.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementtEvent : MonoBehaviour
+{
+    [SerializeField] private Vector2EventChannelSO moveEventChannel;
+
+    [SerializeField] private float speed = 2f;
+
+    private Vector2 moveInput;
+
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.linearVelocity = moveInput * speed;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+}
