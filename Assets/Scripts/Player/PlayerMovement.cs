1ff5b6b4603215ec6a9d25fb867a7105a560292d
tributegames <tributegamestudio@gmail.com>
2025-05-18 19:11:06 +0200

    Update player movement to use Event Channel workflow

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 2f;
-
+    [SerializeField] private Vector2EventChannelSO moveDirectionEventChannel;
+    [SerializeField] private float speed = 5f;
     private Vector2 moveInput;
-
     private Rigidbody2D rb;
-
+    private Collider2D circleCollider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+    }
+
+    void OnEnable()
+    {
+        Helpers.SubscribeIfNotNull(moveDirectionEventChannel, OnMoveDirection);
+    }
+
+    void OnDisable()
+    {
+        Helpers.UnsubscribeIfNotNull(moveDirectionEventChannel, OnMoveDirection);
     }
 
     // Update is called once per frame
@@ -21,13 +33,18 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
+    private void OnMoveDirection(Vector2 moveDirection)
+    {
+        moveInput = moveDirection;
+    }
+
     private void Move()
     {
         rb.linearVelocity = moveInput * speed;
     }
 
-    public void OnMove(InputValue value)
-    {
-        moveInput = value.Get<Vector2>();
-    }
+    // public void OnMove(InputValue value)
+    // {
+    //     moveInput = value.Get<Vector2>();
+    // }
 }
